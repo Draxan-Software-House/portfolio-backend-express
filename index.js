e50f22c69bd696f189155fc5c1711387b84b5371
Andar <50786504+andaaree@users.noreply.github.com>
2025-11-18 14:16:03 +0700

    feat(auth): add urlencoded from express to receive in request body

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 ﻿import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.js";
+import router from "./routes/auth.js";
 
 dotenv.config();
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.json({ status: "OK", message: "Express API up" }));
+app.use("/api/auth", router);
 
-app.use("/api/auth", authRoutes);
-
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
