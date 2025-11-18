7442b805be056fe86013909425b147a8c5475a71
Andar <50786504+andaaree@users.noreply.github.com>
2025-11-18 20:01:21 +0700

    feat(prod): add category model, migration, route, controller

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,3 +14,10 @@ product.get('/',verif,productController.index);
 product.post('/',verif,productController.store);
 product.patch('/:id',verif,productController.update);
 product.delete('/',verif,productController.destroy);
+
+// category routes
+export const category = express.Router();
+category.get('/',verif,categoryController.index);
+category.post('/',verif,categoryController.store);
+category.patch('/:id',verif,categoryController.update);
+category.delete('/',verif,categoryController.destroy);
