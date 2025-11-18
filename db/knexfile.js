cf85eb770fb7c8809776cc2cc8c6313aac36e5eb
Andar <50786504+andaaree@users.noreply.github.com>
2025-11-18 13:54:12 +0700

    fix(auth): add __dirname var to fix no file found db error

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,15 +1,18 @@
 import path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export default {
   client: "sqlite3",
   connection: {
-    filename: path.resolve("./database.sqlite")
+    filename: path.resolve(__dirname,"./database.sqlite")
   },
   useNullAsDefault: true,
   migrations: {
-    directory: path.resolve("./migrations")
+    directory: path.resolve(__dirname,"./migrations")
   },
   seeds: {
-    directory: path.resolve("./seeds")
+    directory: path.resolve(__dirname,"./seeds")
   }
 };
