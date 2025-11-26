0fbbaeda2b27564131ec3f736d519a42ad6f3251
Andar <50786504+andaaree@users.noreply.github.com>
2025-11-26 07:12:40 +0700

    feat(main): add products index response

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,10 +5,20 @@ const resp = (res, code, message) => res.status(code).json({ message });
 const productController = {
   async index(req, res) {
     try {
-      
-      return resp()
+      const page = req.query.page || 0;
+      const prod = await Product.query().page(page,10);
+      const rs = {
+        status:201,
+        message: "Products retrieved successfully"
+      }
+      return resp(res,201,rs);
     } catch (error) {
-      
+      const rs = {
+        status: 500,
+        message: "Error occured",
+        error: error.message
+      }
+      return resp(res,500,rs);
     }
   },
   async store(req, res) {
